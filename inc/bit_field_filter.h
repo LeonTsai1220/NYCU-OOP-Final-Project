1e65efe8b08448eee1dc41febfb20bf55bc2025f
LeonTsai1220 <leon911220@gmail.com>
2024-08-11 14:29:58 +0800

    2024/8/11 filter done

--- inc/bit_field_filter.h
+++ inc/bit_field_filter.h
@@ -1,43 +1,304 @@
 #include <iostream>
-
+#include <cmath>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
 Image* box_filter(Image* img, int kernel) {
     RGBImage* rgbimage = dynamic_cast<RGBImage*>(img);
-    if (!rgbimage) return nullptr;
-
+    GrayImage* grayimage = dynamic_cast<GrayImage*>(img);
     int width = img->get_width();
     int height = img->get_height();
-    int*** pixels = rgbimage->get_pixels();
+    Image* tmp;
 
-    int*** newpixels = new int**[height];
-    for (int c = 0; c < height; c++) {
-        newpixels[c] = new int*[width];
-        for (int i = 0; i < width; i++) {
-            newpixels[c][i] = new int[3];
+    if (rgbimage){
+        int*** pixels = rgbimage->get_pixels();
+        int*** newpixels = new int**[height];
+        for (int c = 0; c < height; c++) {
+            newpixels[c] = new int*[width];
+            for (int i = 0; i < width; i++) {
+                newpixels[c][i] = new int[3];
+            }
         }
-    }
+        for (int c = 0; c < 3; c++) {
+            for (int i = 0; i < height - kernel; i++) {
+                for (int j = 0; j < width - kernel; j++) {
+                    int sum = 0;
+                    int count = 0;
+                    for (int k = i; k < i + kernel; k++) {
+                        for (int t = j; t < j + kernel; t++) {
+                            sum += pixels[k][t][c];
+                            count++;
+                        }
+                    }
+                    int average = sum / count;
+                    for (int k = i; k < i + kernel; k++) {
+                        for (int t = j; t < j + kernel; t++) {
+                            newpixels[k][t][c] = average;
+                        }
+                    }
+                }
+            }
+        }
+        tmp = new RGBImage(width, height, newpixels);
 
     
-    for (int c = 0; c < 3; c++) {
+    }
+    if(grayimage){
+        int** pixels = grayimage->get_pixels();
+        int** newpixels = new int*[height];
+        for (int c = 0; c < height; c++) {
+            newpixels[c] = new int[width];
+        }
+       
         for (int i = 0; i < height - kernel; i++) {
             for (int j = 0; j < width - kernel; j++) {
                 int sum = 0;
                 int count = 0;
                 for (int k = i; k < i + kernel; k++) {
                     for (int t = j; t < j + kernel; t++) {
-                        sum += pixels[k][t][c];
+                        sum += pixels[k][t];
                         count++;
                     }
                 }
                 int average = sum / count;
                 for (int k = i; k < i + kernel; k++) {
                     for (int t = j; t < j + kernel; t++) {
-                        newpixels[k][t][c] = average;
+                        newpixels[k][t] = average;
+                    }
+                }
+            }
+        }    
+        tmp = new GrayImage(width, height, newpixels);
+
+        
+
+    }
+
+
+    
+
+
+    return tmp;
+}
+
+int conv(int A[3][3], int B[3][3]) {
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+           
+           
+            sum += A[i][j] * B[i][j];
+            
+        }
+    }
+    return sum;
+}
+Image* sobel_filter(Image* img, int kernel) {
+    RGBImage* rgbimage = dynamic_cast<RGBImage*>(img);
+    GrayImage* grayimage = dynamic_cast<GrayImage*>(img);
+    int width = img->get_width();
+    int height = img->get_height();
+    int Gx[3][3] ={{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0},  {1, 2, 1}};
+    Image* tmp;
+    int bound = kernel / 2;
+    cout << bound << endl;
+    if(grayimage){
+        int** pixels = grayimage->get_pixels();
+        int** newpixels = new int*[height];
+        for (int c = 0; c < height; c++) {
+            newpixels[c] = new int[width];
+        }
+        
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int sumGx = 0;
+                int sumGy = 0;
+                int matrix[3][3] = {0};
+                //memset(matrix, 0, sizeof(matrix));
+                for (int k = -bound; k <= bound; ++k) {
+                    for (int t = -bound; t <= bound; ++t) {
+                        int y = i + k;
+                        int x = j + t;
+                        if (y >= 0 && y < height && x >= 0 && x < width) {
+                            matrix[k + bound][t + bound] = pixels[y][x];
+                        } else {
+                            matrix[k + bound][t + bound] = 0;
+                        }
+                    }
+                }
+                sumGx = conv(Gx, matrix);
+                sumGy = conv(Gy, matrix);
+                newpixels[i][j] = min(255, static_cast<int>(std::sqrt(sumGx * sumGx + sumGy * sumGy)));
+            }
+        }
+        
+        tmp = new GrayImage(width, height, newpixels);
+
+        
+
+    }
+
+
+    
+
+
+    return tmp;
+}
+
+
+Image* contrast_stretching(Image* img) {
+    RGBImage* rgbimage = dynamic_cast<RGBImage*>(img);
+    GrayImage* grayimage = dynamic_cast<GrayImage*>(img);
+    int width = img->get_width();
+    int height = img->get_height();
+    Image* tmp;
+    if(grayimage){
+        int** pixels = grayimage->get_pixels();
+        int** newpixels = new int*[height];
+        for (int c = 0; c < height; c++) {
+            newpixels[c] = new int[width];
+        }
+        int max = -1, min = 256;
+        for(int i = 0;i < height; i++){
+            for(int j = 0;j<width; j++){
+                if(pixels[i][j] > max){
+                    max = pixels[i][j];
+                }
+                if(pixels[i][j] < min){
+                    min = pixels[i][j];
+                }
+            }
+        }
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                newpixels[i][j] = static_cast<uint8_t>(255.0 * (pixels[i][j] - min) / (max - min));
+            }
+        }
+        
+        tmp = new GrayImage(width, height, newpixels);
+
+        
+
+    }
+
+    if (rgbimage){
+        int*** pixels = rgbimage->get_pixels();
+        int*** newpixels = new int**[height];
+        for (int c = 0; c < height; c++) {
+            newpixels[c] = new int*[width];
+            for (int i = 0; i < width; i++) {
+                newpixels[c][i] = new int[3];
+            }
+        }
+        
+        int max = -1, min = 256;
+        for(int c = 0;c < 3;c++){
+            for(int i = 0;i < height; i++){
+                for(int j = 0;j<width; j++){
+                    if(pixels[i][j][c] > max){
+                        max = pixels[i][j][c];
+                    }
+                    if(pixels[i][j][c] < min){
+                        min = pixels[i][j][c];
                     }
                 }
             }
         }
-    }
+        for(int c = 0;c < 3;c++){
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    newpixels[i][j][c] = static_cast<uint8_t>(255.0 * (pixels[i][j][c] - min) / (max - min));
+                }
+            }
+        }
+        
+        tmp = new RGBImage(width, height, newpixels);
 
-    Image* tmp = new RGBImage(width, height, newpixels);
+        
+
+    }
+    
+
+
     return tmp;
 }
+
+Image* mosaic_filter(Image* img, int kernel) {
+    RGBImage* rgbimage = dynamic_cast<RGBImage*>(img);
+    GrayImage* grayimage = dynamic_cast<GrayImage*>(img);
+    int width = img->get_width();
+    int height = img->get_height();
+    Image* tmp;
+
+    if (rgbimage){
+        int*** pixels = rgbimage->get_pixels();
+        int*** newpixels = new int**[height];
+        for (int c = 0; c < height; c++) {
+            newpixels[c] = new int*[width];
+            for (int i = 0; i < width; i++) {
+                newpixels[c][i] = new int[3];
+            }
+        }
+        for (int c = 0; c < 3; c++) {
+            for (int i = 0; i < height; i+=kernel) {
+                for (int j = 0; j < width; j+=kernel) {
+                    int sum = 0;
+                    int count = 0;
+                    for (int k = i; k < i + kernel && k < height; k++) {
+                        for (int t = j; t < j + kernel && t < width; t++) {
+                            sum += pixels[k][t][c];
+                            count++;
+                        }
+                    }
+                    int average = sum / count;
+                    for (int k = i; k < i + kernel && k < height; k++) {
+                        for (int t = j; t < j + kernel && t < width; t++) {
+                            newpixels[k][t][c] = average;
+                        }
+                    }
+                }
+            }
+        }
+        tmp = new RGBImage(width, height, newpixels);
+
+    
+    }
+    if(grayimage){
+        int** pixels = grayimage->get_pixels();
+        int** newpixels = new int*[height];
+        for (int c = 0; c < height; c++) {
+            newpixels[c] = new int[width];
+        }
+       
+        for (int i = 0; i < height; i+=kernel) {
+            for (int j = 0; j < width; j+=kernel) {
+                int sum = 0;
+                int count = 0;
+                for (int k = i; k < i + kernel && k < height; k++) {
+                    for (int t = j; t < j + kernel && t < width; t++) {
+                        sum += pixels[k][t];
+                        count++;
+                    }
+                }
+                int average = sum / count;
+                for (int k = i; k < i + kernel && k < height; k++) {
+                    for (int t = j; t < j + kernel && t < width; t++) {
+                        newpixels[k][t] = average;
+                    }
+                }
+            }
+        }    
+        tmp = new GrayImage(width, height, newpixels);
+
+        
+
+    }
+
+
+    
+
+
+    return tmp;
+}
+
--- main.cpp
+++ main.cpp
@@ -11,12 +11,12 @@ int main(int argc, char *argv[]){
     
         
     Image *img1 = new GrayImage();
-    if(img1->LoadImage("Image-Folder/mnist/img_100.jpg")){
+    if(img1->LoadImage("Image-Folder/img6.png")){
         cout << "Load successfully!" << endl;
     }
     img1->DumpImage("img1.jpg");
     img1->Display_X_server();
-    img1->Display_ASCII();
+    //img1->Display_ASCII();
     //img1->Display_CMD("Image-Folder/mnist/img_100.jpg");
     
 
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]){
     img2->LoadImage("Image-Folder/lena.jpg");
     img2->DumpImage("img2.jpg");
     img2->Display_X_server();
-    img2->Display_ASCII();
+    //img2->Display_ASCII();
     //img2->Display_CMD();
     cout << "box filter" << endl;
     Image *img3 = new RGBImage();
@@ -33,9 +33,41 @@ int main(int argc, char *argv[]){
     cout << "dump img3" << endl;
     img3->DumpImage("img3.jpg");
     img3->Display_X_server();
+
+
+    Image *img4 = new GrayImage();
+    img4 = box_filter(img1, 10);
+    cout << "dump img4" << endl;
+    img4->DumpImage("img4.jpg");
+    img4->Display_X_server();
+
+
+    Image *img5 = new GrayImage();
+    img5 = sobel_filter(img1, 3);
+    img5->DumpImage("img5.jpg");
+    img5->Display_X_server();
+
+
+    Image * img6 = new GrayImage();
+    img6 = contrast_stretching(img1);
+    img6->DumpImage("img6.jpg");
+    img6->Display_X_server();
+
+    Image * img7 = new RGBImage();
+    img7 = contrast_stretching(img2);
+    img7->DumpImage("img7.jpg");
+    img7->Display_X_server();
     // some bit field filter design driven code here
+    Image * img8 = new GrayImage();
+    img8 = mosaic_filter(img1, 5);
+    img8->DumpImage("img8.jpg");
+    img8->Display_X_server();
 
     // some photo mosaic driven code here
+    Image * img9 = new RGBImage();
+    img9 = mosaic_filter(img2, 5);
+    img9->DumpImage("img9.jpg");
+    img9->Display_X_server();
 
     // more ...
     return 0;
